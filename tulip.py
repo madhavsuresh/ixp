1b73a6d247e5fd0f1a4ba5b375e5c315e14c1e47
Madhav <madman92@gmail.com>
2012-02-25 19:45:03 -0600

    handled errors

--- tulip.py
+++ tulip.py
@@ -6,6 +6,7 @@ import re
 import json
 import sys
 import os
+import unicodedata
 
 endpoint = 'http://www-wanmon.slac.stanford.edu/cgi-wrap/tulip-viz.cgi?target=v3vee.org'
 
@@ -15,22 +16,35 @@ class Tulip():
     _server_header = ['city','country','ip','min_rtt','a_rtt','max_rtt','loss','type','teir','distance']
 
     #input host name, output coordinates and servers that were hit
-    def getHostInfo(self,name):
-        print name
-        f = open('log/' + name,'w')
+    def getHostInfo(self,raw_data,name):
+        #print name
+        #f = open('log/' + name,'w')
+        '''
         sock = urllib.urlopen(self._tulip_endpoint+name)
         raw_data = sock.read()
         f.write(raw_data)
         sock.close()
         f.close()
+        '''
         if 'Unable to get 3 minimun rtt values'  in raw_data:
-            print 'Unable to triangulate host %s' % (name)
+            print '%s :ERROR Unable to triangulate host' % (name)
             return None
+        elif 'Error: ping timeout' in raw_data:
+            print '%s:ERROR Ping timeout' % (name)
+            return None
+        elif len(raw_data) == 0:
+            print '%s: ERROR empty file' %(name)
+            return None
+        
 
         location = self.getLocation(raw_data)
-        servers = self.getServers(raw_data)
-        print 'Coordinates: %s, %s. Address: %s' % (location['coords'][0],location['coords'][1],location['address'])
-        print servers
+        if not location:
+            print 'ERROR: Geoip not found'
+            return None
+        else:
+            print 'Coordinates: %s, %s. Address: %s' % (location['coords'][0],location['coords'][1],location['address'])
+        #servers = self.getServers(raw_data)
+        #print servers
 
     # get in raw_data, return list of servers that were used to geolocate
     def getServers(self,raw_data):
@@ -44,27 +58,29 @@ class Tulip():
 
     #given raw data, returns coordinates and address of queried address
     def getLocation(self,raw_data):
-        reg = re.compile(r'L\w+ through GEOIP is\s+?:\s+?(-?\d+\.\d+)')
+        reg = re.compile(r'L\w+ through GEOIP is\s+?:\s+?(-?\d+\.?\d*)')
         coords = reg.findall(raw_data)
+
+        if len(coords) == 0:
+            return None
+
         maps = urllib.urlopen(self._maps_endpoint+coords[0] + ',' + coords[1])
+
         map_data = json.loads(maps.read())
         address_string = map_data['results'][0]['formatted_address']
+        address_string = unicodedata.normalize('NFKD', address_string).encode('ascii','ignore')
         return {'coords':coords,'address':address_string}
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
 
-        try:
-            os.mkdir('log')
-        except OSError:
-            print 'log directory exists, will overwrite existing files..'
-        t = Tulip()
-        f = open(sys.argv[1],'r')
+    files = os.listdir('log')
+    t = Tulip()
 
-        for line in f:
-            t.getHostInfo(line.strip(' \n'))
-    else:
-        print 'usage: python tulip.py list_of_servers'
+    for f in files:
+        of = open('log/' + f,'r')
+        raw_data = of.read()
+
+        t.getHostInfo(raw_data,f)
 
 
 
